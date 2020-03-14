dabf9dfdc186f20904edbfbbd713e5fe99788513
kunge2013 <543976974@qq.com>
2020-03-14 21:37:15 +0800

    1

---
+++ gateway/eurakegateway/src/main/java/com/kframe/gateway/EurekaGateWayApplication.java
@@ -0,0 +1,18 @@
+package com.kframe.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaGateWayApplication {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(EurekaGateWayApplication.class);
+
+    public static void main(String[] args) {
+    	SpringApplication.run(EurekaGateWayApplication.class);
+    }
+}
--- gateway/eurakegateway/src/test/java/com/kframe/gateway/EurekaclientApplicationTests.java
+++ gateway/eurakegateway/src/test/java/com/kframe/gateway/EurekaclientApplicationTests.java
@@ -1,4 +1,4 @@
-package com.kframe.eurekaclient;
+package com.kframe.gateway;
 
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
