5837800dd0391c683525efa72477511432c41173
kunge2013 <543976974@qq.com>
2020-03-17 18:27:05 +0800

    2121

---
+++ NetFlix/eurekaauthservce/src/main/java/com/kframe/auth/MyGrantedAuthority.java
@@ -0,0 +1,13 @@
+package com.kframe.auth;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class MyGrantedAuthority implements GrantedAuthority {
+
+	@Override
+	public String getAuthority() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ NetFlix/eurekaauthservce/src/main/java/com/kframe/auth/config/WebSecurityConfig.java
@@ -0,0 +1,7 @@
+package com.kframe.auth.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+}
