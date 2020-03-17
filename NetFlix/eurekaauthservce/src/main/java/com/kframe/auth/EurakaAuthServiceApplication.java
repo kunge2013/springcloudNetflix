3f89ba66a68103c5a006268b792fe2c1e0193484
kunge2013 <543976974@qq.com>
2020-03-17 18:26:38 +0800

    2121

--- NetFlix/eurekaauthservce/src/main/java/com/kframe/auth/EurakaAuthServiceApplication.java
+++ NetFlix/eurekaauthservce/src/main/java/com/kframe/auth/EurakaAuthServiceApplication.java
@@ -3,11 +3,14 @@ package com.kframe.auth;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class EurakaAuthServiceApplication {
 
 	static final Logger LOGGER = LoggerFactory.getLogger(EurakaAuthServiceApplication.class);
