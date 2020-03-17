50f78b012a6c934248bc9be8271212cca67b2f3d
kunge2013 <543976974@qq.com>
2020-03-17 14:11:15 +0800

    1212

--- gateway/eurakegateway/src/main/java/com/kframe/gateway/AuthFilter.java
+++ gateway/eurakegateway/src/main/java/com/kframe/gateway/AuthFilter.java
@@ -8,6 +8,8 @@ import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
@@ -18,7 +20,6 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
-import io.lettuce.core.dynamic.annotation.CommandNaming;
 import reactor.core.publisher.Mono;
 /**
  * 过滤器配置
@@ -33,6 +34,8 @@ public class AuthFilter implements GlobalFilter, Ordered {
 	 */
 	@Resource
 	private IAuthSevice authSevice;
+	
+
 	/**
 	 * 日志处理
 	 */
--- gateway/eurakegateway/src/main/java/com/kframe/gateway/JwtConstant.java
+++ gateway/eurakegateway/src/main/java/com/kframe/gateway/JwtConstant.java
@@ -11,4 +11,8 @@ public class JwtConstant {
 	public static final int JWT_TTL = 3600000;
 
 	public static final int JWT_REFRESH_INTERVAL = 3300000;
+	
+	
+	public static final int JWT_REFRESH_TTL = 350000;
+	
 }
