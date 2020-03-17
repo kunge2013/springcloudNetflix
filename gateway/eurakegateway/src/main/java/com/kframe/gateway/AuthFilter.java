cb4a58e8dc3ef6a8a1fb687937d13d0ea988d9e9
kunge2013 <543976974@qq.com>
2020-03-17 17:01:03 +0800

    2121

--- gateway/eurakegateway/src/main/java/com/kframe/gateway/AuthFilter.java
+++ gateway/eurakegateway/src/main/java/com/kframe/gateway/AuthFilter.java
@@ -41,7 +41,7 @@ public class AuthFilter implements GlobalFilter, Ordered {
 	private IAuthSevice authSevice;
 
 	private Serializable fetchSerializableId(ServerWebExchange exchange) {
-        String auth =exchange.getRequest().getHeaders().getFirst("Authorization");
+        String auth = exchange.getRequest().getHeaders().getFirst("Authorization");
         Serializable userId = null;
         if ((auth != null) && (auth.length() > 7)) {
         	Optional<Claims> optional = JwtFactory.parseJWT(auth);
@@ -64,48 +64,14 @@ public class AuthFilter implements GlobalFilter, Ordered {
         ServerHttpRequest request = exchange.getRequest();
         // 响应对象
         ServerHttpResponse response = exchange.getResponse();
-
         Serializable userid = fetchSerializableId(exchange);
         LOGGER.info("userid is {}", userid);
-        // 只有综合路由才添加这个全局过滤器（routesId：route_all）
-        // 如果请求路径中不存在 routeAll 字符串
-//        if(request.getURI().toString().indexOf("routeAll") == -1) {
-//        	LOGGER.info("filter -> return");
-//            // 直接跳出
-//            return chain.filter(exchange);
-//        }
-//
-//        // 从请求中获取 token 参数
-//        String token = exchange.getRequest().getQueryParams().getFirst("token");
-//        // 如果为空，那么将返回 401
-//        if (token == null || token.isEmpty()) {
-//            // 响应消息内容对象
-//            Map<String, Object> message = new HashMap();
-//            // 响应状态 
-//            message.put("code", -1);
-//            // 响应内容
-//            message.put("msg", "缺少凭证");
-//            // 转换响应消息内容对象为字节
-//            byte[] bits = message.toString().getBytes(StandardCharsets.UTF_8);
-//            DataBuffer buffer = response.bufferFactory().wrap(bits);
-//            // 设置响应对象状态码 401
-//            response.setStatusCode(HttpStatus.UNAUTHORIZED);
-//            // 设置响应对象内容并且指定编码，否则在浏览器中会中文乱码
-//            response.getHeaders().add("Content-Type", "text/plain;charset=UTF-8");
-//            // 返回响应对象
-//            return response.writeWith(Mono.just(buffer));
-//        }
-//        // 获取请求地址
-//        String beforePath = request.getPath().pathWithinApplication().value();
-//        // 获取响应状态码
-//        HttpStatus beforeStatusCode = response.getStatusCode();
-//        System.out.println("响应码：" + beforeStatusCode + "，请求路径：" + beforePath);
-//        // 请求前
-//        System.out.println("filter -> before");
-        // 如果不为空，就通过
         return chain.filter(exchange);
     }
 
+	public static void main(String[] args) {
+		System.out.println(JwtFactory.createJWT("test", 1));
+	}
 	@Override
 	public int getOrder() {
 		// TODO Auto-generated method stub
