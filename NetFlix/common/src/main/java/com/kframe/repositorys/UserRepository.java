633154c6c54d5decdd30a490dd85e59a95b31d1e
kunge2013 <543976974@qq.com>
2020-03-18 09:57:17 +0800

    12121

--- NetFlix/common/src/main/java/com/kframe/repositorys/UserRepository.java
+++ NetFlix/common/src/main/java/com/kframe/repositorys/UserRepository.java
@@ -1,11 +1,13 @@
 package com.kframe.repositorys;
 
+import java.io.Serializable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.kframe.entity.UserInfo;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserInfo, String> {
+public interface UserRepository extends JpaRepository<UserInfo, Serializable> {
 
 }
